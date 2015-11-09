47f4ef07b3f87e0d8ba971b37b79a09ab8e6dd08
fiatjaf@spooner <fiatjaf@gmail.com>
2015-11-09 15:22:35 -0500

    merging batches from different sublevels. with tests.

--- sublevel.go
+++ sublevel.go
@@ -1,17 +1,31 @@
+// Copyright (c) 2015, Giovanni T. Parra <fiatjaf@gmail.com>
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 package sublevel
 
 import (
+	"log"
+
 	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/syndtr/goleveldb/leveldb/iterator"
 	"github.com/syndtr/goleveldb/leveldb/opt"
 	"github.com/syndtr/goleveldb/leveldb/util"
-	"log"
 )
 
 /* basic db management */
 
+/*  opens an abstract DB for the given file.
+    the abstract DB is said to be "abstract" because it cannot
+    do any operation, it is there only to generate sublevels.
+
+    It is not possible to access the underlying leveldb with this
+    package.
+*/
 func OpenFile(dbfile string, o *opt.Options) AbstractLevel {
-	db, err := leveldb.OpenFile(dbfile, nil)
+	db, err := leveldb.OpenFile(dbfile, o)
 	return AbstractLevel{
 		leveldb: db,
 		err:     err,
@@ -125,59 +139,93 @@ func (si SubIterator) Error() error {
 }
 
 /* transactions */
+type AbstractBatchOperation struct {
+	kind  string // PUT or DELETE
+	key   []byte
+	value []byte
+}
+
+//  starts a new batch write for the specified sublevel.
 func (s Sublevel) NewBatch() *SubBatch {
-	return &SubBatch{
-		namespace: s.namespace,
-		batch:     new(leveldb.Batch),
-	}
+	return &SubBatch{namespace: s.namespace}
 }
 
 type SubBatch struct {
 	namespace []byte
-	batch     *leveldb.Batch
+	ops       []AbstractBatchOperation
 }
 
 func (b *SubBatch) Delete(key []byte) {
 	key = append(append([]byte(nil), b.namespace...), key...)
-	b.batch.Delete(key)
+	b.ops = append(b.ops, AbstractBatchOperation{"DELETE", key, nil})
 }
 
 func (b *SubBatch) Put(key []byte, value []byte) {
 	key = append(append([]byte(nil), b.namespace...), key...)
-	b.batch.Put(key, value)
+	b.ops = append(b.ops, AbstractBatchOperation{"PUT", key, value})
 }
 
 func (b *SubBatch) Dump() []byte {
-	return b.batch.Dump()
+	return makeBatchWithOps(b.ops).Dump()
 }
 
 func (b *SubBatch) Len() int {
-	return b.batch.Len()
+	return len(b.ops)
 }
 
 func (b *SubBatch) Reset() {
-	b.batch.Reset()
+	b.ops = make([]AbstractBatchOperation, 0)
 }
 
-func (s Sublevel) Write(b *SubBatch, wo *opt.WriteOptions) (err error) {
-	return s.db.Write(b.batch, wo)
+func (s Sublevel) Write(b *SubBatch, wo *opt.WriteOptions) error {
+	return s.db.Write(makeBatchWithOps(b.ops), wo)
 }
 
 /* transactions on different stores */
 func (a AbstractLevel) NewBatch() *SuperBatch {
-	return &SuperBatch{
-		batch: new(leveldb.Batch),
-	}
+	return &SuperBatch{}
 }
 
 type SuperBatch struct {
-	batch *leveldb.Batch
+	ops []AbstractBatchOperation
 }
 
 func (sb *SuperBatch) MergeSubBatch(b *SubBatch) {
-
+	sb.ops = append(sb.ops, b.ops...)
 }
 
-func (a AbstractLevel) Write(sb *SuperBatch, wo *opt.WriteOptions) (err error) {
-	return a.leveldb.Write(sb.batch, wo)
+func (sb *SuperBatch) Dump() []byte {
+	return makeBatchWithOps(sb.ops).Dump()
+}
+
+func (sb *SuperBatch) Len() int {
+	return len(sb.ops)
+}
+
+func (sb *SuperBatch) Reset() {
+	sb.ops = make([]AbstractBatchOperation, 0)
+}
+
+func (a AbstractLevel) Write(sb *SuperBatch, wo *opt.WriteOptions) error {
+	return a.leveldb.Write(makeBatchWithOps(sb.ops), wo)
+}
+
+func (a AbstractLevel) MultiBatch(subbatches ...*SubBatch) *SuperBatch {
+	sb := a.NewBatch()
+	for _, b := range subbatches {
+		sb.MergeSubBatch(b)
+	}
+	return sb
+}
+
+func makeBatchWithOps(ops []AbstractBatchOperation) *leveldb.Batch {
+	batch := new(leveldb.Batch)
+	for _, op := range ops {
+		if op.kind == "PUT" {
+			batch.Put(op.key, op.value)
+		} else if op.kind == "DELETE" {
+			batch.Delete(op.key)
+		}
+	}
+	return batch
 }
